229569a2dc9e6a65d13adf62bcb05788bbf5a921
Beata Bajko <beata.sowa1@gmail.com>
2016-10-30 18:40:51 +0100

    wypisywanie wartości

--- main.cpp
+++ main.cpp
@@ -2,23 +2,43 @@
 #include <pthread.h>
 
 using namespace std;
+pthread_mutex_t blokada;
+int z= 0;
+
+
 void* test(void* t)
 {
-	cout<<"HELLO";
-	return NULL;	
+	pthread_mutex_lock(&blokada);
+	
+	int* k = (int*)t;
+	cout<< *k << endl;
+	(*k)++;
+	
+	pthread_mutex_unlock(&blokada);
+	
+	pthread_exit(NULL);	
 	
 }
 
 int main()
 {
 	int n;
-	cout <<"Podaj ile wątków";
+	cout <<"Podaj ile wątków: ";
 	cin >>n;
 	pthread_t watki[n];
 	
-	for (int i=0; i<n; i++)
+	for ( int i=0; i<n; i++)
+	{
 		//int pthread create(pthread t *id, const pthread attr t *attr, void* (fun*)(void*), void* arg)
-		pthread_create(&watki[i], NULL, test, NULL);
+		pthread_create(&watki[i], NULL, test, &z);
+	}
+	
+	for(int i=0; i<n; i++)
+	{
+		pthread_join(watki[i],NULL);
+	}
+	
+	cout<<"Hello"<<endl;
 
   return 0;
 }
